a1e59c5f92d6a2eac097a217852b3a1d9d929492
Artem Paliutin <pal.artem@icloud.com>
2023-03-10 16:33:21 +0300

    Initial Commit

---
+++ DemoProfile/DemoProfileApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoProfileApp.swift
+//  DemoProfile
+//
+//  Created by Artem Paliutin on 10/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct DemoProfileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
